2c4a57605b1a81b53e1fddce037ae9fccd3325a9
Ayush Singh <ayush.singh.96@gmail.com>
2016-04-02 18:28:48 +0530

    2 april
    user registration complete

--- app/User.php
+++ app/User.php
@@ -12,7 +12,7 @@ class User extends Authenticatable
      * @var array
      */
     protected $fillable = [
-        'name', 'email', 'password',
+        'first_name','last_name', 'email', 'password','college_id', 'branch_id', 'start_year', 'end_year', 'address', 'contact',
     ];
 
     /**
--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -10,25 +10,40 @@
                     <form class="form-horizontal" role="form" method="POST" action="{{ url('/register') }}">
                         {!! csrf_field() !!}
 
-                        <div class="form-group{{ $errors->has('name') ? ' has-error' : '' }}">
-                            <label class="col-md-4 control-label">Name</label>
+                        <div class="form-group{{ $errors->has('first_name') ? ' has-error' : '' }}">
+                            <label class="col-md-4 control-label">First Name</label>
 
                             <div class="col-md-6">
-                                <input type="text" class="form-control" name="name" value="{{ old('name') }}">
+                                <input type="text" class="form-control" placeholder="Should have atleast 2 characters" name="first_name" value="{{ old('firstname') }}" required>
 
-                                @if ($errors->has('name'))
+                                @if ($errors->has('first_name'))
                                     <span class="help-block">
-                                        <strong>{{ $errors->first('name') }}</strong>
+                                        <strong>{{ $errors->first('first_name') }}</strong>
                                     </span>
                                 @endif
                             </div>
                         </div>
 
+                        <div class="form-group{{ $errors->has('last_name') ? ' has-error' : '' }}">
+                            <label class="col-md-4 control-label">Last Name</label>
+
+                            <div class="col-md-6">
+                                <input type="text" class="form-control" placeholder="Should have atleast 2 characters" name="last_name" value="{{ old('last_name') }}" required>
+
+                                @if ($errors->has('last_name'))
+                                    <span class="help-block">
+                                        <strong>{{ $errors->first('last_name') }}</strong>
+                                    </span>
+                                @endif
+                            </div>
+                        </div>
+
+
                         <div class="form-group{{ $errors->has('email') ? ' has-error' : '' }}">
                             <label class="col-md-4 control-label">E-Mail Address</label>
 
                             <div class="col-md-6">
-                                <input type="email" class="form-control" name="email" value="{{ old('email') }}">
+                                <input type="email" class="form-control" name="email" value="{{ old('email') }}" placeholder="Enter E-Mail Address" required>
 
                                 @if ($errors->has('email'))
                                     <span class="help-block">
@@ -42,7 +57,7 @@
                             <label class="col-md-4 control-label">Password</label>
 
                             <div class="col-md-6">
-                                <input type="password" class="form-control" name="password">
+                                <input type="password" class="form-control" name="password" placeholder="Should contain atleast 6 characters" required>
 
                                 @if ($errors->has('password'))
                                     <span class="help-block">
@@ -56,7 +71,7 @@
                             <label class="col-md-4 control-label">Confirm Password</label>
 
                             <div class="col-md-6">
-                                <input type="password" class="form-control" name="password_confirmation">
+                                <input type="password" class="form-control" name="password_confirmation" placeholder="Re-Enter Password" required>
 
                                 @if ($errors->has('password_confirmation'))
                                     <span class="help-block">
@@ -65,11 +80,112 @@
                                 @endif
                             </div>
                         </div>
+                    <div class="form-group{{ $errors->has('branch_id') ? ' has-error' : '' }}">
+                            <label class="col-md-4 control-label">Branch</label>
+
+                            <div class="col-md-6">
+                                <select name="branch_id" class="form-control" id="branch_id" value="{{ old('branch_id') }}" required>
+                                <option value="">----SELECT BRANCH----</option>
+                                @foreach($branches as $branch)
+                                <option value="{{ $branch->id }}">{{ $branch->name }}</option>
+                                @endforeach
+                                </select>
+                                @if ($errors->has('branch_id'))
+                                    <span class="help-block">
+                                        <strong>{{ $errors->first('branch_id') }}</strong>
+                                    </span>
+                                @endif
+                            </div>
+                        </div>
+
+
+                        <div class="form-group{{ $errors->has('college_id') ? ' has-error' : '' }}">
+                            <label class="col-md-4 control-label">College</label>
+
+                        <div class="col-md-6">
+                                <select name="college_id" class="form-control" id="college_id" value="{{ old('college_id') }}" required>
+                                <option value="">----SELECT COLLEGE----</option>
+                                @foreach($colleges as $college)
+                                <option value="{{ $college->id }}">{{ $college->name }}</option>
+                                @endforeach
+                                </select>
+                                @if ($errors->has('college_id'))
+                                    <span class="help-block">
+                                        <strong>{{ $errors->first('college_id') }}</strong>
+                                    </span>
+                                @endif
+                            </div>
+                        </div>
+
+                        <div class="form-group{{ $errors->has('start_year') ? ' has-error' : '' }}">
+                            <label class="col-md-4 control-label">Start year</label>
+
+                            <div class="col-md-6">
+                                <select class="form-control"  name="start_year" value="{{ old('start_year') }}" required>
+                                <option selected="selected">----Enter Year of Admission----</option>
+                                <option value ="{{ \Carbon\Carbon::now()->format('Y') }}">{{ \Carbon\Carbon::now()->format('Y') }}</option>
+                                <option value ="{{ \Carbon\Carbon::now()->format('Y')-1 }}">{{ \Carbon\Carbon::now()->format('Y')-1 }}</option>
+                                <option value ="{{ \Carbon\Carbon::now()->format('Y')-2 }}">{{ \Carbon\Carbon::now()->format('Y')-2 }}</option>
+                                <option value ="{{ \Carbon\Carbon::now()->format('Y')-3 }}">{{ \Carbon\Carbon::now()->format('Y')-3 }}</option>
+                                </select>
+                                @if ($errors->has('start_year'))
+                                    <span class="help-block">
+                                        <strong>{{ $errors->first('start_year') }}</strong>
+                                    </span>
+                                @endif
+                            </div>
+                        </div>
+
+                          <div class="form-group{{ $errors->has('duration') ? ' has-error' : '' }}">
+                            <label class="col-md-4 control-label">Duration</label>
+
+                            <div class="col-md-6">
+                                <select class="form-control" min="1" max="4" name="duration" value="{{ old('duration') }}" required>
+                                <option selected="selected">----Ex. 4 Years for B.Tech----</option>
+                                <option value="1">1 year</option>
+                                <option value="2">2 years</option>
+                                <option value="3">3 years</option>
+                                <option value="4">4 years</option>
+                                </select>
+                                @if ($errors->has('duration'))
+                                    <span class="help-block">
+                                        <strong>{{ $errors->first('duration') }}</strong>
+                                    </span>
+                                @endif
+                            </div>
+                        </div>
+
+                         <div class="form-group{{ $errors->has('address') ? ' has-error' : '' }}">
+                            <label class="col-md-4 control-label">Address</label>
+                            <div class="col-md-6">
+                                <textarea rows="10" cols="50" name="address" class="form-control" required>
+                                </textarea>
+                                @if ($errors->has('address'))
+                                    <span class="help-block">
+                                        <strong>{{ $errors->first('address') }}</strong>
+                                    </span>
+                                @endif
+                            </div>
+                        </div>
+
+                            <div class="form-group{{ $errors->has('contact') ? ' has-error' : '' }}">
+                            <label class="col-md-4 control-label">Contact No</label>
+
+                            <div class="col-md-6">
+                                <input type="text" class="form-control" placeholder="Enter your contact number" name="contact" required>
+
+                                @if ($errors->has('contact'))
+                                    <span class="help-block">
+                                        <strong>{{ $errors->first('contact') }}</strong>
+                                    </span>
+                                @endif
+                            </div>
+                        </div>
 
                         <div class="form-group">
                             <div class="col-md-6 col-md-offset-4">
                                 <button type="submit" class="btn btn-primary">
-                                    <i class="fa fa-btn fa-user"></i>Register
+                                    <i class="fa fa-btn fa-user"></i>Join Us!
                                 </button>
                             </div>
                         </div>
@@ -80,3 +196,10 @@
     </div>
 </div>
 @endsection
+@if($errors->any())
+<ul>
+@foreach($errors->all() as $error)
+<li>{{ $error }}</li>
+@endforeach
+</ul>
+@endif
